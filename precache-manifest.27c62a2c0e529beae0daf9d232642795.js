62df3c693bb6c98c19cd02710da7bd4f139cfd46
kotepagu <kotepagu@kotepagu.local>
2020-08-16 17:58:53 +0900

    Updates

---
+++ precache-manifest.27c62a2c0e529beae0daf9d232642795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2bfdf5e1d10844d999fff075f20653",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4fbfc8d7459330e2eabe",
+    "url": "./static/css/main.1c8a9ec6.chunk.css"
+  },
+  {
+    "revision": "3cf332d620e3afcd8527",
+    "url": "./static/js/2.6d79f073.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6d79f073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fbfc8d7459330e2eabe",
+    "url": "./static/js/main.35af91e6.chunk.js"
+  },
+  {
+    "revision": "594d4d545ccbaccf3e6f",
+    "url": "./static/js/runtime-main.4aa7f8ea.js"
+  }
+]);
